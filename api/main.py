5343afa30639e872350e0487cd160c2297c42d52
askchandan <chandanmalakar6209@gmail.com>
2025-12-14 16:03:22 +0530

    db env access given to api and tested - OK

--- api/main.py
+++ api/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import psycopg2
+import os
 
 app = FastAPI()
 
+def get_db_connection():
+    return psycopg2.connect(
+        host="db",
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+    )
+
 @app.get("/")
 def health():
-    return {"status": "DocuStack API running"}
+    try:
+        conn = get_db_connection()
+        conn.close()
+        return {"status": "API running and DB connected"}
+    except Exception as e:
+        return {"status": "API running but DB not reachable", "error": str(e)}
